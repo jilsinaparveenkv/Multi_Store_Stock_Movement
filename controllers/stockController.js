6e99eca8a8bc303fb1ae5564c8797287f3ec443f
jilsina <jilsinamkd@gmail.com>
2026-07-15 00:01:11 +0530

    stoke productus quatity transfering between stores and stoke quantity not moving assured negative sections fixed

--- controllers/stockController.js
+++ controllers/stockController.js
@@ -1,3 +1,4 @@
+const mongoose = require("mongoose");
 const Stock = require("../models/Stock");
 const Product = require("../models/Product");
 const Store = require("../models/Store");
@@ -78,8 +79,178 @@ const getStocks = async (req, res) => {
     });
   }
 };
+// adjust Stock
+const adjustStock = async (req, res) => {
+  try {
+    const { productId, storeId, quantity } = req.body;
+
+    if (!productId || !storeId || quantity === undefined) {
+      return res.status(400).json({
+        message: "Product, store and quantity are required",
+      });
+    }
+
+    if (quantity === 0) {
+      return res.status(400).json({
+        message: "Quantity cannot be zero",
+      });
+    }
+
+    let updatedStock;
+
+    // Increase stock
+    if (quantity > 0) {
+      updatedStock = await Stock.findOneAndUpdate(
+        {
+          product: productId,
+          store: storeId,
+        },
+        {
+          $inc: { quantity: quantity },
+        },
+        {
+          new: true,
+        },
+      );
+    }
+
+    // Decrease stock
+    else {
+      updatedStock = await Stock.findOneAndUpdate(
+        {
+          product: productId,
+          store: storeId,
+          quantity: { $gte: Math.abs(quantity) },
+        },
+        {
+          $inc: { quantity: quantity },
+        },
+        {
+          new: true,
+        },
+      );
+    }
+
+    if (!updatedStock) {
+      return res.status(400).json({
+        message: "Insufficient stock or stock record not found",
+      });
+    }
+
+    res.status(200).json({
+      message: "Stock adjusted successfully",
+      stock: updatedStock,
+    });
+  } catch (error) {
+    res.status(500).json({
+      message: error.message,
+    });
+  }
+};
+//auto transfering stoke between stores, store-store stoke moving
+const transferStock = async (req, res) => {
+  const session = await mongoose.startSession();
+
+  try {
+    session.startTransaction();
+
+    const { productId, sourceStoreId, destinationStoreId, quantity } = req.body;
+
+    if (!productId || !sourceStoreId || !destinationStoreId || !quantity) {
+      await session.abortTransaction();
+      session.endSession();
+
+      return res.status(400).json({
+        message: "All fields are required",
+      });
+    }
+
+    if (quantity <= 0) {
+      await session.abortTransaction();
+      session.endSession();
+
+      return res.status(400).json({
+        message: "Quantity must be greater than zero",
+      });
+    }
+
+    if (sourceStoreId === destinationStoreId) {
+      await session.abortTransaction();
+      session.endSession();
+
+      return res.status(400).json({
+        message: "Source and destination store cannot be the same",
+      });
+    }
+
+    // Ddduct stock from source store
+    const sourceStock = await Stock.findOneAndUpdate(
+      {
+        product: productId,
+        store: sourceStoreId,
+        quantity: { $gte: quantity },
+      },
+      {
+        $inc: {
+          quantity: -quantity,
+        },
+      },
+      {
+        new: true,
+        session,
+      },
+    );
+
+    if (!sourceStock) {
+      await session.abortTransaction();
+      session.endSession();
+
+      return res.status(400).json({
+        message: "Insufficient stock in source store",
+      });
+    }
+
+    // add stock to destination store
+    let destinationStock = await Stock.findOne({
+      product: productId,
+      store: destinationStoreId,
+    }).session(session);
+
+    if (destinationStock) {
+      destinationStock.quantity += quantity;
+      await destinationStock.save({ session });
+    } else {
+      destinationStock = await Stock.create(
+        [
+          {
+            product: productId,
+            store: destinationStoreId,
+            quantity,
+          },
+        ],
+        { session },
+      );
+    }
+
+    await session.commitTransaction();
+    session.endSession();
+
+    res.status(200).json({
+      message: "Stock transferred successfully",
+    });
+  } catch (error) {
+    await session.abortTransaction();
+    session.endSession();
+
+    res.status(500).json({
+      message: error.message,
+    });
+  }
+};
 
 module.exports = {
   createStock,
   getStocks,
+  adjustStock,
+  transferStock,
 };
--- routes/stockRoutes.js
+++ routes/stockRoutes.js
@@ -2,14 +2,22 @@ const express = require("express");
 
 const router = express.Router();
 
-const { createStock, getStocks } = require("../controllers/stockController");
+const {
+  createStock,
+  getStocks,
+  adjustStock,
+  transferStock,
+} = require("../controllers/stockController");
 
 const protect = require("../middleware/authMiddleware");
 const isAdmin = require("../middleware/adminMiddleware");
 
 // admin only
-router.post("/", protect, isAdmin, createStock);
-
+router.post("/", protect, isAdmin, createStock, );
+//for not going negative quantity
+router.patch("/adjust", protect, isAdmin, adjustStock);
+//auto stoke transfer,store-store stoke moving
+router.post("/transfer", protect, isAdmin, transferStock);
 // logged in users
 router.get("/", protect, getStocks);
 
